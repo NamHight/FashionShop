1dae224c0ec388afaba64bdf95fe0bdfc232e5bc
NamHight <finsr8280@gmail.com>
2024-11-26 15:38:51 +0700

    Change rule git ignore

--- FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
+++ FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59f40063072c71a2f26bb6837ed4ec17b7c4ee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ff2a49bd27b50f8fc5fa0c7dafa1427b0e73de")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
