48e10d311699799e384ba3242fc32901670db1d8
loc <locbmt12309>
2024-11-26 09:09:41 +0700

    contact list

---
+++ FashionShop-Admin/Services/Contacts/ContactService.cs
@@ -0,0 +1,17 @@
+﻿using FashionShop.Models;
+using FashionShop.Repositories.ManagerRepo;
+
+namespace FashionShop.Services.Contacts
+{
+    public class ContactService:IContactService
+    {
+        private readonly IManagerRepository _managerRepository;
+        public ContactService(IManagerRepository managerRepository) => _managerRepository = managerRepository;
+
+        public async Task<List<Contact>> GetAllAsync(bool trackChanges)
+        {
+            var contacts = await _managerRepository.Contact.GetAllAsync(trackChanges);
+            return contacts;
+        }
+    }
+}
---
+++ FashionShop-Admin/Services/Contacts/IContactService.cs
@@ -0,0 +1,9 @@
+﻿using FashionShop.Models;
+
+namespace FashionShop.Services.Contacts
+{
+    public interface IContactService
+    {
+        Task<List<Contact>> GetAllAsync(bool trackChanges);
+    }
+}
