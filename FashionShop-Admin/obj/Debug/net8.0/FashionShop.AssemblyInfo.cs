35aef0cf4098a4c8946b03aee8736eaa373698d4
Tunn <166986627+tunn1005@users.noreply.github.com>
2024-12-05 11:09:15 +0700

    update order ver2

--- FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
+++ FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c4003dfe3743fbfa05a84eb5f19826ddd2dff3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ed47e0e26a903a8a44b312978725930c5dca47")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
