b48354a6649cf10d4f0aff5ac0feed6c167bc654
NamHight <finsr8280@gmail.com>
2024-12-05 09:43:19 +0700

    updae debug

--- FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
+++ FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0412548742875c53fe9bf9ca47f40d16c593334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50b7c5c5e8026bc43091d736c64870cc18ad2a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
