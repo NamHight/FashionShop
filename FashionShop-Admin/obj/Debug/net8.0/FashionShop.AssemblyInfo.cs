1f175e295f323fc416b2e7e3ec895a969c9682a5
loc <locbmt12309>
2024-12-05 11:20:15 +0700

    after merge

--- FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
+++ FashionShop-Admin/obj/Debug/net8.0/FashionShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1994065cc5e6c8bb8d2cafa497b8cbb2a022fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c60c0d3218d418de3c7b47f96d17507372d6c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
