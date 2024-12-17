c6511ba425d7d7ef0bd48a95f1c6fff7988bbefe
NamHight <finsr8280@gmail.com>
2024-12-18 00:04:51 +0700

    commit add Paginationn

--- FashionShop-API/Repositories/Categories/IRepositoryCategory.cs
+++ FashionShop-API/Repositories/Categories/IRepositoryCategory.cs
@@ -1,4 +1,5 @@
 ﻿using FashionShop_API.Models;
+using FashionShop_API.Repositories.Shared;
 
 namespace FashionShop_API.Repositories.Categories;
 
@@ -6,4 +7,8 @@ public interface IRepositoryCategory
 {
     Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges);
     Task<Category?> GetCategoryByIdAsync(long id, bool trackChanges);
+    
+    Task<PagedListAsync<Category>> GetAllPaginatedAsync(int page, int limit);
+    Task CreateAsync(Category category);
+    Task<bool> CheckCategorySlugExistsAsync(string slug);
 }
