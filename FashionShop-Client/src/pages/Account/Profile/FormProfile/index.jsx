61a6b503b7d31ef720df096fe2dbeb8afb117ca5
Tunn <166986627+tunn1005@users.noreply.github.com>
2025-01-03 02:14:44 +0700

    commit de pull 002

--- FashionShop-Client/src/pages/Account/Profile/FormProfile/index.jsx
+++ FashionShop-Client/src/pages/Account/Profile/FormProfile/index.jsx
@@ -1,10 +1,24 @@
 import { Radio, Typography } from "@material-tailwind/react";
-import { IoMale, IoFemale, IoMaleFemale  } from "react-icons/io5";
+import { IoMale, IoFemale, IoMaleFemale } from "react-icons/io5";
 import { useState } from "react";
-function FormProfile({user}) {
+function FormProfile({ user }) {
   const [update, setUpdate] = useState(false);
   console.log("adads", user);
-  
+  const handleDate = (birth) => {
+    if (!update) {
+      return <p>{birth}</p>;
+    } else {
+      return (
+        <input
+          type="date"
+          name="Birthday"
+          id="Birthday"
+          className="block w-full rounded-md bg-white p-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+          disabled={!update}
+        />
+      );
+    }
+  };
   return (
     <div className="mx-28">
       <form action="/account" method="post" className="text-lg text-slate-700">
@@ -38,7 +52,7 @@ function FormProfile({user}) {
               type="email"
               name="Email"
               id="Email"
-              placeholder="Tuấn..."
+              placeholder={user.email}
               className="block w-full rounded-md bg-white p-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               disabled={!update}
             />
@@ -56,7 +70,7 @@ function FormProfile({user}) {
               type="text"
               name="Phone"
               id="Phone"
-              placeholder="Tuấn..."
+              placeholder={user.phone}
               className="block w-full rounded-md bg-white p-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               disabled={!update}
             />
@@ -74,7 +88,7 @@ function FormProfile({user}) {
               type="text"
               name="Address"
               id="Address"
-              placeholder="Tuấn..."
+              placeholder={user.address}
               className="block w-full rounded-md bg-white p-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               disabled={!update}
             />
@@ -87,57 +101,72 @@ function FormProfile({user}) {
           >
             Gender
           </label>
-          {!update ? <p className="text-xl flex items-center">Male <IoMale className="text-blue-500 ml-1" size={23}/></p> : <Radio>
-            <div className="flex items-center gap-2">
-              <Radio.Item id="Male">
-                <Radio.Indicator />
-              </Radio.Item>
-              <Typography as="label" htmlFor="Male" className="text-foreground flex items-center">
-                Male <IoMale className="text-blue-500 ml-1"/>
-              </Typography>
-            </div>
-            <div className="flex items-center gap-2">
-              <Radio.Item id="Female">
-                <Radio.Indicator />
-              </Radio.Item>
-              <Typography
-                as="label"
-                htmlFor="Female"
-                className="text-foreground flex items-center"
-              >
-                Female <IoFemale className="text-red-500 ml-1"/>
-              </Typography>
-            </div>
-            <div className="flex items-center gap-2">
-              <Radio.Item id="Another">
-                <Radio.Indicator />
-              </Radio.Item>
-              <Typography
-                as="label"
-                htmlFor="Another"
-                className="text-foreground flex items-center"
-              >
-                Another <IoMaleFemale className="ml-1"/>
-              </Typography>
-            </div>
-          </Radio>}
+          {!update ? (
+            <p className="text-xl flex items-center">
+              {user.gender === "male" ? (
+                <>
+                  Male <IoMale className="text-blue-500 ml-1" size={23} />
+                </>
+              ) : user.gender === "female" ? (
+                <>
+                  Female <IoFemale className="text-blue-500 ml-1" size={23} />
+                </>
+              ) : (
+                <>
+                  Another
+                  <IoMaleFemale className="text-blue-500 ml-1" size={23} />
+                </>
+              )}{" "}
+            </p>
+          ) : (
+            <Radio>
+              <div className="flex items-center gap-2">
+                <Radio.Item id="Male">
+                  <Radio.Indicator />
+                </Radio.Item>
+                <Typography
+                  as="label"
+                  htmlFor="Male"
+                  className="text-foreground flex items-center"
+                >
+                  Male <IoMale className="text-blue-500 ml-1" />
+                </Typography>
+              </div>
+              <div className="flex items-center gap-2">
+                <Radio.Item id="Female">
+                  <Radio.Indicator />
+                </Radio.Item>
+                <Typography
+                  as="label"
+                  htmlFor="Female"
+                  className="text-foreground flex items-center"
+                >
+                  Female <IoFemale className="text-red-500 ml-1" />
+                </Typography>
+              </div>
+              <div className="flex items-center gap-2">
+                <Radio.Item id="Another">
+                  <Radio.Indicator />
+                </Radio.Item>
+                <Typography
+                  as="label"
+                  htmlFor="Another"
+                  className="text-foreground flex items-center"
+                >
+                  Another <IoMaleFemale className="ml-1" />
+                </Typography>
+              </div>
+            </Radio>
+          )}
         </div>
-        <div className="my-5">
+        <div className="my-5 flex items-center">
           <label
             htmlFor="Birthday"
-            className="block text-xl text-gray-900 font-bold"
+            className="block text-xl text-gray-900 font-bold mr-5"
           >
             Birthday
           </label>
-          <div className="mt-2">
-            <input
-              type="date"
-              name="Birthday"
-              id="Birthday"
-              className="block w-full rounded-md bg-white p-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              disabled={!update}
-           />
-          </div>
+          {handleDate(user.birth)}
         </div>
         <div className="text-center flex">
           {!update ? (
@@ -157,7 +186,11 @@ function FormProfile({user}) {
             </button>
           )}
           {update && (
-            <button type="button" className="border-2 w-full px-5 bg-red-500 text-white py-3 rounded-full" onClick={() => setUpdate(!update)}>
+            <button
+              type="button"
+              className="border-2 w-full px-5 bg-red-500 text-white py-3 rounded-full"
+              onClick={() => setUpdate(!update)}
+            >
               Huỷ
             </button>
           )}
