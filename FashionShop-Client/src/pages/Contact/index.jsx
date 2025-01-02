a8f7af829f7975ac55eb54fd70758b14825311b8
anhthuan <zzzzthuan81@gmail.com>
2025-01-02 16:59:58 +0700

    page contact

--- FashionShop-Client/src/pages/Contact/index.jsx
+++ FashionShop-Client/src/pages/Contact/index.jsx
@@ -1,8 +1,102 @@
 import React from "react";
 
-const Contact = () =>{
-    return (
-        <div>Hello contact</div>
-    )
-}
+const Contact = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-col-2 grid-flow-col gap-4">
+        <div className="col-span-6">
+          <form>
+            <div className="border-b border-gray-900/10 pb-12">
+              <h2 className="text-base/7 font-semibold text-gray-900">
+                Liên hệ
+              </h2>
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="col-span-6">
+                  <label
+                    htmlFor="username"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    FullName
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                      <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder=""
+                        className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-6">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-span-6">
+                  <label
+                    htmlFor="phone"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Phone
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="phone"
+                      name="phone"
+                      type="number"
+                      autoComplete="phone"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+                <div className="col-span-6">
+                  <label
+                    htmlFor="about"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Description
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="about"
+                      name="about"
+                      rows={3}
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="col-span-6"></div>
+      </div>
+    </div>
+  );
+};
 export default Contact;
