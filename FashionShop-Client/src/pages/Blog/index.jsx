2df55f7d212e5cb44fa0226da15febbc9ba0df5f
anhthuan <zzzzthuan81@gmail.com>
2025-01-03 23:52:58 +0700

    function getPromotions in page blog

--- FashionShop-Client/src/pages/Blog/index.jsx
+++ FashionShop-Client/src/pages/Blog/index.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllPromotion } from "../../services/api/PromotionService";
+import { keyframes } from "motion";
 
 const Blog = () => {
+  const [promotions, setPromotions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const pageNumber = 1;
+  const getPromotions = async (page) => {
+    const result = await getAllPromotion({
+      params: {
+        page: page,
+        limit: 9,
+      },
+    });
+    console.log(result.data.item1);
+    setPromotions(result.data.item1);
+    setCurrentPage(result.data.item2.currentPage);
+    setTotalPage(result.data.item2.totalPages);
+
+    console.log("promotion ", promotions); // Kiểm tra khi promotions thay đổi
+  };
+  useEffect(() => {
+    getPromotions(currentPage);
+  }, [currentPage]);
+
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="col-span-1">1</div>
-        <div className="col-span-1">1</div>
-        <div className="col-span-1">1</div>
-        <div className="col-span-1">1</div>
-        <div className="col-span-1">1</div>
-        <div className="col-span-1">1</div>
+        {promotions && promotions.length > 0 ? (
+          promotions.map((item, key) => {
+            return (
+              <div className="col-span-1" key={key}>
+                {item.promotionName}
+              </div>
+            );
+          })
+        ) : (
+          <p className="col-span-1">Dữ liệu đang xử lý</p>
+        )}
       </div>
       <div className="flex items-center justify-end">
         <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 px-6">
           <div className="flex flex-1 items-center justify-between">
-            <div>
-              <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                <span className="">Previous</span>
-              </a>
-              <a href="#" className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                <span className="">1</span>
-              </a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                <span className="">2</span>
-              </a>
-              <a href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                <span className="">Next</span>
-              </a>
+            <div className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              Previous
+            </div>
+            {/* { totalPage ? (
+              [...Array(totalPage)].map((_, key) => {
+                const currentNumber = pageNumber++;
+                return (
+                <div key={key} className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">{currentNumber}</div>
+                );
+              })
+            ) : (
+              <div>1</div>
+            )
+            } */}
+
+            <div className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              Next
             </div>
           </div>
         </div>
--- FashionShop-Client/src/services/api/PromotionService.jsx
+++ FashionShop-Client/src/services/api/PromotionService.jsx
@@ -2,8 +2,8 @@ import { authAxios } from "../../libs/Config/AxiosConfig";
 const END_POINT = {
     GetPromotion : "/Promotions",
 }
-export const getAllPromotion = (paramPage) => {
-    return authAxios.get(`${END_POINT.GetPromotion}`,paramPage);
+export const getAllPromotion = (paramPromotionDto) => {
+    return authAxios.get(`${END_POINT.GetPromotion}`,paramPromotionDto);
 }
 export const getByIdPromotion = (id) =>{
     return authAxios.get(`${END_POINT.GetPromotion}/${id}`);
