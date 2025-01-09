301f864cc73b589124a35b8cd9b110f9c5627284
LongCan0912 <longcan5664@gmail.com>
2025-01-09 11:26:16 +0700

    Add not found at home page

--- FashionShop-Client/src/components/Product/ProductList.jsx
+++ FashionShop-Client/src/components/Product/ProductList.jsx
@@ -22,7 +22,13 @@ const ProductList = () => {
   }, []);
 
   if (loading) return <Loading/>;
-  if (error) return <div>{error}</div>;
+  if (error) return <div class="bg-gray-100 px-2 text-center">
+  <div class="h-screen flex flex-col justify-center items-center">
+      <h1 class="text-8xl font-extrabold text-red-500">500</h1>
+      <p class="text-4xl font-medium text-gray-800">Internal Server Error</p>
+      <p class="text-xl text-gray-800 mt-4">We apologize for the inconvenience. Please try again later.</p>
+  </div>
+</div>;
 
   return (
     <div className="max-w-screen-xl mx-auto p-4">
