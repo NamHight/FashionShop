c412f0c1dbb69606397823c928e79f7bdcc47a27
LongCan0912 <longcan5664@gmail.com>
2025-01-13 14:03:14 +0700

    Add LatestProducts component at homePage

--- FashionShop-Client/src/pages/Home/Index.jsx
+++ FashionShop-Client/src/pages/Home/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ProductList from "../../components/List/ProductList";
 import AutoBanner from '../../components/AutoBanner/AutoBanner';
 import CategoriesList from '../../components/List/CategoryList';
+import LatestProductList from '../../components/List/LatestProductsList';
 const Home = () => {
     return (
     <div>
     <AutoBanner />
     <h1 className="text-3xl font-bold mb-6">Categories List</h1>
     <CategoriesList/>
+    <LatestProductList/>
     <ProductList />
     </div>
     );
